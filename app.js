55ac96555dfe34721d2ab92b002fe5e0e0525c55
Youcef <youcefcopy@gmail.com>
2025-11-19 18:20:10 +0100

    eventListener for the submit btn

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ function openNoteDialog() {
     const dialog = document.getElementById('noteDialog');
     const titleInput = document.getElementById('titleInput');
     const contentInput = document.getElementById('contentInput');
-
     // this opens the dialog
     dialog.showModal();
     titleInput.focus();
-
 }
 
 function closeNoteDialog() {
     const dialog = document.getElementById('noteDialog');
-    
-    
     dialog.close();
 }
+
+// every time the website loads, it's gonna call this
+// we have to use the function keyword, this refers to the document
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('noteForm').addEventListener('submit',saveNote);
+    document.getElementById('noteDialog').addEventListener('click',function (event) {
+        if (event.target === this) {
+            closeNoteDialog();
+        }
+    })
+})
 
 const themeToggleBtn = document.getElementById('themeToggleBtn');
 themeToggleBtn.addEventListener('click', 
