3274238e8c151e29e791b60d416e7b779f8d9aaa
Youcef <youcefcopy@gmail.com>
2025-11-19 17:23:45 +0100

    js | array for notes, and openNotedialog function

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('running');
+// create a new array for notes
+// each not will be saved as an object inside that array
+let notes = [];
+
+function openNoteDialog() {
+    const dialog = document.getElementById('noteDialog');
+    const titleInput = document.getElementById('titleInput');
+    const contentInput = document.getElementById('contentInput');
+
+}
